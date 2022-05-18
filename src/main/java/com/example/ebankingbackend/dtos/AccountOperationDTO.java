c035d8218a9cc2960192fe8a2cef8d82c8ef36a9
Sedyame Hamza <hamzasedyame61@gmail.com>
2022-05-18 16:45:21 +0100

    Second Commit

--- src/main/java/com/example/ebankingbackend/dtos/AccountOperationDTO.java
+++ src/main/java/com/example/ebankingbackend/dtos/AccountOperationDTO.java
@@ -1,5 +1,6 @@
-package com.example.ebankingbackend.entities;
+package com.example.ebankingbackend.dtos;
 
+import com.example.ebankingbackend.entities.BankAccount;
 import com.example.ebankingbackend.enums.OperationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,19 +8,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class AccountOperation {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+@Data
+public class AccountOperationDTO {
+    private Long id;
     private Date operationDate;
     private double amount;
-    @Enumerated(EnumType.STRING)
     private OperationType type;
-    @ManyToOne
-    private BankAccount bankAccount;
     private String description;
 }
